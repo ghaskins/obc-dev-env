c4ee3c3670d72822fab710db5c4538ef80985f7e
Gregory Haskins <gregory.haskins@gmail.com>
2016-03-11 22:06:09 -0500

    Use the ubuntu-provided virtualbox guest additions rather than the packer engine provided
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- baseimage/scripts/vagrant/virtualbox.sh
+++ baseimage/scripts/vagrant/virtualbox.sh
@@ -2,9 +2,4 @@
 
 set -x
 
-mkdir -p /mnt/virtualbox
-mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
-sh /mnt/virtualbox/VBoxLinuxAdditions.run
-ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
-umount /mnt/virtualbox
-rm -rf /home/vagrant/VBoxGuest*.iso
+apt-get install --yes virtualbox-guest-utils
